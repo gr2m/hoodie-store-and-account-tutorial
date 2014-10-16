007f71fa889a760b8ab5e473e5a5f08dc011ff07
Gregor Martynus <gregor@martynus.net>
2014-10-16 16:26:54 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hoodie Store & Account Tutorial
+
+This is a simple app with instructions that you can follow
+to learn all the basis about `hoodie.store` and `hoodie.account.
+
+## Installation & start
+
+```
+hoodie new -t hoodie-store-and-account-tutorial
+cd hoodie-store-and-account-tutorial
+```
+
+Once installation is done, you can start the app with
+
+```
+hoodie start
+# will ask you for an admin password on first run
+```
---
+++ www/assets/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// show inline code
+$('.content script').each(function() {
+  var code = $.trim( $(this).text() );
+  $(this).after('<pre><code class="language-javascript">'+code+'</code></pre>')
+})
+
+// add pagination
+var numPages = 5;
+var currentPage = parseInt(location.pathname.substr(1)) || 1;
+var pagination = '<ul class="pagination">\n';
+pagination += '  <li><a href="/">1</a></li>\n';
+
+for (var i = 2; i <= numPages; i++) {
+  pagination += '  <li><a href="/'+i+'.html">'+i+'</a></li>\n';
+};
+
+pagination += '</ul>';
+var $pagination = $(pagination)
+$pagination.find('li').eq(currentPage-1).addClass('active');
+$('.content').prepend($pagination).append($pagination.clone())
+
